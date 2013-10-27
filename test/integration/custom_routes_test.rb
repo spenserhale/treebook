f0267e2a93917c2fea3cf2e60a11c0c509c17968
Spenser Hale <sphale@live.com>
2013-10-27 13:59:28 -0700

    Updated Profile and added scoped status update s

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -13,4 +13,8 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
      get '/register'
      assert_response :success
    end
+  test "that a profile page works"  do
+    get '/spenserhale'
+    assert_response :success
+  end
 end
